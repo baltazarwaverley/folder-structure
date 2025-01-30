c0dcd01f19d36952dd47f98e1c33ef3c47991aec
baltazarwaverley <mbaltazar@waverleysoftware.com>
2025-01-30 14:32:02 -0400

    refactor: implement refactor to the Folder component

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,42 +1,30 @@
-import { useCallback, useState } from 'react';
 
+import { useCallback, useState } from 'react';
 import { IFileNodeProps } from '../constants/interfaces';
 import Icon from './Icon';
 
-
 const Folder = ({ file }: IFileNodeProps) => {
 
-  const [fileName, fileExtension] = file.name.split('.');
-  const [showChildren, setShowChildren] = useState(false);
+  const [name, extension] = file.name.split('.')
+  const [showItems, setShowItem] = useState<boolean | null>(null)
 
-  const onExpandedHandler = useCallback(() => {
-    setShowChildren(!showChildren)
-  }, [showChildren, setShowChildren])
+  const onShowItems = useCallback(() => {
+    setShowItem(!showItems);
+  }, [setShowItem, showItems])
 
   return (
     <div>
-      <div
-        className={`hover:cursor-pointer hover:bg-gray-900 hover:rounded-sm text-lg flex flex-row items-center`}
-        onClick={onExpandedHandler}
-      >
-        {file.isFolder ?
-          <>
-            <Icon name={showChildren ? 'caretDown' : 'caretRight'} className='size-4' /><span>{fileName}</span>
-          </>
-          :
-          <>
-            <Icon name={fileExtension} className='size-4' /><span>{file.name}</span>
-          </>
+      <div className='flex flex-row items-center justify-start rounded-md px-2 hover:bg-gray-900 hover:cursor-pointer' onClick={onShowItems}>
+        {
+          file.isFolder ? <Icon name={showItems ? 'caretDown' : 'caretRight'} className='size-4' /> : <Icon name={extension} className='size-4' />
         }
+        <span className='px-1'>{file.name}</span>
       </div>
-
-      {file.isFolder && file.children && (
-        <div>
-          {showChildren && file.children.map((child, index) => (
-            <Folder key={index} file={child} />
-          ))}
-        </div>
-      )}
+      <div className='ml-4'>
+        {file.isFolder && file.children && showItems && file.children.map((item, index) => {
+          return <Folder key={file.name + index} file={item} />
+        })}
+      </div>
     </div>
   )
 };
