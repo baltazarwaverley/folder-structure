f45d94ebaa4d3c3a73647b0fde79788f8b0799ef
baltazarwaverley <mbaltazar@waverleysoftware.com>
2025-01-29 21:01:50 -0400

    feat: integrate data mock to render the components correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css'
+import CodeEditor from './components/CodeEditor';
+import { files } from './constants/data';
+import Folder from './components/Folder';
+// import './App.css';
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-red-500 text-cyan-200">
-        Welcome to folder structure
-      </h1>
-    </>
-  )
+    <div className="p-5 bg-gray-950 min-h-screen flex flex-row overflow-hidden">
+      <div className='w-1/4 bg-gray-950 border border-gray-700 text-gray-200 p-5'>
+        <Folder file={files} />
+      </div>
+      <div className="w-3/4 bg-gray-900 border border-gray-700 flex justify-center items-center">
+        <CodeEditor />
+      </div>
+    </div>
+  );
 }
-
-export default App
